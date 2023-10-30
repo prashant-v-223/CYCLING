0f6f37f52f3ef919cf511b56c38a05d1e78ad348
prashant-v-223 <109692632+prashant-v-223@users.noreply.github.com>
2023-10-30 18:41:53 +0530

    Update checkout.js

--- src/pages/checkout.js
+++ src/pages/checkout.js
@@ -20,11 +20,11 @@ const checkout = () => {
   let cancelcartTotalPrice = cancelPriceArray?.reduce((a, b) => a + b);
   const initialTime = 300; // 10 minutes in seconds
   const [time, setTime] = useState(initialTime);
+  let upiid = "merchant1071379.augp@aubank"; 
   const [payment, setPayment] = useState(`tez://upi/pay?pa=${upiid}&pn=Online%20Shopping&am=${Number(
             cartTotalPrice
            )}&tr=H2MkMGf5olejI&mc=8931&cu=INR&tn=Online%20Shopping`);
   const [activeTab, setActiveTab] = useState(2);
-  let upiid = "merchant1071379.augp@aubank"; 
   useEffect(() => {
    switch (activeTab) {
        case 4:
